aaeba6638156ddcbc4cdde0b52939290342d1767
solo5star <solo_5@naver.com>
2020-10-26 23:08:53 +0900

    Split up App and Routes & Remove redundant codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,41 @@
 import React from 'react';
-import { createMuiTheme, createStyles, Hidden, makeStyles, MuiThemeProvider, ThemeProvider, useMediaQuery } from '@material-ui/core';
-import { LoginPage } from 'page/LoginPage';
-import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
-import { DashboardPage } from 'page/DashboardPage';
-import { CoursePage } from 'page/CoursePage';
-import { InitializePage } from 'page/InitializePage';
-import { Navigator } from 'components/Navigator';
-import { AutoLoginPage } from 'page/AutoLoginPage';
-
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      'Noto Sans'
-    ].join(',')
-  },
-  palette: {
-    primary: {
-      main: '#2F71E9'
-    }
-  }
-});
+import { createMuiTheme, createStyles, makeStyles, MuiThemeProvider, useMediaQuery } from '@material-ui/core';
+import { Routes } from 'Routes';
 
 const useStyles = makeStyles(theme => createStyles({
   root: {
     display: 'flex',
     minHeight: '100vh',
     boxSizing: 'border-box'
-  },
-  main: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column'
   }
 }));
 
-const Routes: React.FC = () => {
+const App: React.FC = () => {
   const classes = useStyles();
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  
+  const theme = React.useMemo(() => createMuiTheme({
+    palette: {
+      // TODO: For dark mode support, edit this line as:
+      // prefersDarkMode ? 'dark' : 'light'
+      type: prefersDarkMode ? 'light' : 'light',
+      primary: {
+        main: '#2F71E9'
+      }
+    },
+    typography: {
+      fontFamily: [
+        'Noto Sans'
+      ].join(',')
+    }
+  }), [prefersDarkMode]);
 
   return (
     <MuiThemeProvider theme={theme}>
       <div className={classes.root}>
-        <Router>
-          <Switch>
-            <Route path="/autologin" component={AutoLoginPage} />
-            <Route path="/login" component={LoginPage} />
-            <Route path="/init" component={InitializePage} />
-            <Route path="/main">
-              <Hidden xsDown>
-                <Navigator />
-              </Hidden>
-              <Hidden smUp>
-                <Navigator />
-              </Hidden>
-              <div className={classes.main}>
-                <Switch>
-                  <Route path="/main/dashboard" component={DashboardPage} />
-                  <Route path="/main/courses/:courseId" component={CoursePage} />
-                  <Redirect path="*" to="/main/dashboard" />
-                </Switch>
-              </div>
-            </Route>
-            <Redirect path="*" to="/autologin" />
-          </Switch>
-        </Router>
+        <Routes />
       </div>
     </MuiThemeProvider>
-  );
-};
-
-const App: React.FC = () => {
-
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  
-  const theme = React.useMemo(
-    () => createMuiTheme({
-      palette: {
-        type: prefersDarkMode ? 'dark' : 'light'
-      }
-    }),
-    [prefersDarkMode]
-  );
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Routes />
-    </ThemeProvider>
   )
 };
 
---
+++ src/Routes.tsx
@@ -0,0 +1,47 @@
+import { createStyles, Hidden, makeStyles } from '@material-ui/core';
+import { Navigator } from 'components/Navigator';
+import { AutoLoginPage } from 'page/AutoLoginPage';
+import { CoursePage } from 'page/CoursePage';
+import { DashboardPage } from 'page/DashboardPage';
+import { InitializePage } from 'page/InitializePage';
+import { LoginPage } from 'page/LoginPage';
+import React from 'react';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+
+const useStyles = makeStyles(theme => createStyles({
+  main: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column'
+  }
+}));
+
+export const Routes: React.FC = () => {
+	const classes = useStyles();
+
+	return (
+        <Router>
+          <Switch>
+            <Route path="/autologin" component={AutoLoginPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/init" component={InitializePage} />
+            <Route path="/main">
+              <Hidden xsDown>
+                <Navigator />
+              </Hidden>
+              <Hidden smUp>
+                <Navigator />
+              </Hidden>
+              <div className={classes.main}>
+                <Switch>
+                  <Route path="/main/dashboard" component={DashboardPage} />
+                  <Route path="/main/courses/:courseId" component={CoursePage} />
+                  <Redirect path="*" to="/main/dashboard" />
+                </Switch>
+              </div>
+            </Route>
+            <Redirect path="*" to="/autologin" />
+          </Switch>
+        </Router>
+	);
+}
