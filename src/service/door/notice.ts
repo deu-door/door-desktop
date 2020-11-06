1af24e5c1c499b97df1b986109ee2aa830748033
solo5star <solo_5@naver.com>
2020-11-07 05:43:00 +0900

    Refactor notice.ts

--- src/service/door/notice.ts
+++ src/service/door/notice.ts
@@ -46,27 +46,19 @@ export async function getNotices(courseId: ID): Promise<FetchableMap<Notice>> {
 
 	const table = document(`#sub_content2 > div.form_table > table`).toArray().pop();
 
-	if(!table) throw new Error('공지사항을 불러올 수 없습니다. 로그인 상태를 확인해주세요.');
+	if(!table) throw new Error('공지사항 목록을 불러올 수 없습니다. 로그인 상태를 확인해주세요.');
 
-	/**
-	 * 번호: "알림" | "2",
-	 * 읽음: "",
-	 * 제목: "실시간 ZOOM 수업 안내",
-	 * 작성자: "홍길동",
-	 * 등록일: "2020-09-15",
-	 * 조회: 32
-	 */
-	const notices = parseTableElement(table).map(notice => ({
-		id: notice['제목'].url?.match(/CourseNotice\/(\w+)?/)?.[1],
+	const notices: Notice[] = parseTableElement(table).map(row => ({
+		id: row['제목'].url?.match(/CourseNotice\/(\w+)?/)?.[1] || '',
 		courseId: courseId,
 
-		author: notice['작성자'].text,
-		createdAt: new Date(notice['등록일'].text),
-		title: notice['제목'].text,
-		views: Number(notice['조회'].text),
+		author: row['작성자'].text,
+		createdAt: new Date(row['등록일'].text),
+		title: row['제목'].text,
+		views: Number(row['조회'].text),
 
 		...notFulfilledFetchable()
-	} as Notice));
+	}));
 
 	return {
 		items: Object.fromEntries(notices.map(notice => [notice.id, notice])),
