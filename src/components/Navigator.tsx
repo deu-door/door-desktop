b112b3e98cffb15d0b70a65a341505c14e68aa5f
solo5star <solo_5@naver.com>
2020-11-07 20:36:46 +0900

    Styling Navigator

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -5,65 +5,68 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import HomeIcon from '@material-ui/icons/Home';
 import { useSelector } from 'react-redux';
 import { Course } from 'service/door/interfaces/course';
 import { CourseState } from 'store/modules/courses';
-import { useHistory } from 'react-router';
-import { Drawer, ListSubheader } from '@material-ui/core';
+import { useHistory, useLocation } from 'react-router';
+import { Drawer, Grid, ListSubheader, SvgIcon, Typography } from '@material-ui/core';
 import { RootState } from 'store/modules';
+import { ReactComponent as DoorIcon } from 'resources/logo-original-white.svg';
+import clsx from 'clsx';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => createStyles({
     drawer: {
-      width: drawerWidth
+      width: drawerWidth,
+      background: '#18202c',
+      color: 'rgba(255, 255, 255, 0.7)'
+    },
+    divider: {
+      background: '#404854'
     },
     category: {
+      color: theme.palette.common.white,
       fontWeight: 'bold'
     },
-    categoryHeaderPrimary: {
+    header: {
+      backgroundColor: '#232f3e',
       color: theme.palette.common.white,
+      '&:hover,&:focus': {
+        backgroundColor: 'rgba(255, 255, 255, 0.08)'
+      }
+    },
+    headerIcon: {
+      width: '3rem',
+      height: '3rem'
+    },
+    headerText: {
+      fontWeight: 'bold'
     },
     item: {
-      paddingTop: 1,
-      paddingBottom: 1,
-      color: 'rgba(255, 255, 255, 0.7)',
+      color: 'rgba(255, 255, 255, 0.8)',
       '&:hover,&:focus': {
         backgroundColor: 'rgba(255, 255, 255, 0.08)',
       },
+      paddingTop: theme.spacing(1),
+      paddingBottom: theme.spacing(1)
     },
-    itemCategory: {
-      // backgroundColor: '#232f3e',
-      // backgroundColor: '#2F71E9',
-      // boxShadow: '0 -1px 0 #404854 inset',
-      paddingTop: theme.spacing(2),
-      paddingBottom: theme.spacing(2),
+    itemActive: {
+      color: '#4fc3f7'
     },
-    firebase: {
-      fontSize: 24,
-	  color: theme.palette.common.white,
-	  paddingTop: theme.spacing(4)
+    itemTitle: {
+      fontWeight: 'bolder'
     },
-    itemActiveItem: {
-      color: '#4fc3f7',
-    },
-    itemPrimary: {
-      fontSize: 'inherit',
-    },
-    itemIcon: {
-      minWidth: 'auto',
-      marginRight: theme.spacing(2),
-    },
-    divider: {
-      marginTop: theme.spacing(2),
-    },
+    itemSubtitle: {
+      color: 'rgba(255, 255, 255, 0.7)'
+    }
   }));
 
 export const Navigator: React.FC = props => {
   const classes = useStyles();
 
   const history = useHistory();
+  const location = useLocation();
   const courses = useSelector<RootState, CourseState>(state => state.courses);
   
   const coursesByCategory: { [key: string]: Course[] } = {};
@@ -76,27 +79,56 @@ export const Navigator: React.FC = props => {
 
   return (
     <Drawer variant="permanent" ModalProps={{ keepMounted: true }} className={classes.drawer} classes={{ paper: classes.drawer }}>
-      <List>
-        <ListItem button onClick={() => history.push('/main')}>
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <ListItemText>
-            Door Desktop
-          </ListItemText>
+      <List disablePadding>
+        <ListItem
+          button
+          className={classes.header}
+          onClick={() => history.push('/main/timeline')}
+        >
+          <Grid container spacing={3} alignItems="center">
+            <Grid item>
+              <SvgIcon viewBox="0 0 245 245" className={classes.headerIcon}>
+                <DoorIcon />
+              </SvgIcon>
+            </Grid>
+            <Grid
+              item
+              style={{ flex: 1 }}
+              container
+              direction="column"
+              className={classes.headerText}
+            >
+              <Grid item>
+                <Typography variant="h4">Door</Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1">Desktop</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
         </ListItem>
       </List>
       {Object.keys(coursesByCategory).map(category => (
         <React.Fragment key={category}>
-          <Divider />
-          <List subheader={<ListSubheader component="div" className={classes.category}>{category}</ListSubheader>}>
-            {coursesByCategory[category].map(({ id, name, professor }) => (
-              <ListItem key={name} button onClick={() => history.push('/main/courses/' + id)}>
-                <ListItemText primary={name} secondary={professor}>
-                  
+          <Divider className={classes.divider} />
+          <List subheader={
+            <ListSubheader component="div" className={classes.category}>{category}</ListSubheader>
+          }>
+            {coursesByCategory[category].map(({ id, name, professor }) => {
+              const path = '/main/courses/' + id;
+
+              return (<ListItem
+                key={name}
+                button
+                className={clsx(classes.item, location.pathname === path && classes.itemActive)}
+                onClick={() => history.push(path)}
+              >
+                <ListItemText>
+                  <Typography variant="subtitle1" className={classes.itemTitle}>{name}</Typography>
+                  <Typography variant="subtitle2" className={classes.itemSubtitle}>{professor}</Typography>
                 </ListItemText>
-              </ListItem>
-            ))}
+              </ListItem>);
+            })}
           </List>
         </React.Fragment>
       ))}
