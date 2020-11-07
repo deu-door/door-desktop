269067a0e5248fde6d4a5190db330f6cb4f0ed08
solo5star <solo_5@naver.com>
2020-11-07 16:33:12 +0900

    Add post tag component

--- src/components/PostComponent.tsx
+++ src/components/PostComponent.tsx
@@ -1,10 +1,10 @@
-import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, createStyles, Divider, Grid, Link, List, ListItem, ListItemIcon, ListItemText, makeStyles, TextField, Typography } from '@material-ui/core';
+import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, createStyles, Divider, Grid, Link, Icon, makeStyles, Paper, PaperProps, TextField, Typography } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { Attachment, Post } from 'service/door/interfaces';
 import { FetchableAction } from 'store/modules';
 import { DateTime } from './DateTime';
 import { FetchButton } from './FetchButton';
-import { Attachment as AttachmentIcon } from '@material-ui/icons';
+import { Announcement, AssignmentLate, Attachment as AttachmentIcon, Description, OndemandVideo } from '@material-ui/icons';
 import { actions } from 'store/modules';
 import { Assignment } from 'service/door/interfaces/assignment';
 import { Notice } from 'service/door/interfaces/notice';
@@ -12,10 +12,13 @@ import { Lecture } from 'service/door/interfaces/lecture';
 import { downloader } from 'service/downloader';
 import { Reference } from 'service/door/interfaces/reference';
 import VisibilitySensor from 'react-visibility-sensor';
+import { deepOrange, indigo, purple, teal } from '@material-ui/core/colors';
+import clsx from 'clsx';
 
 const useStyles = makeStyles(theme => createStyles({
 	post: {
-		margin: theme.spacing(2, 0)
+		margin: theme.spacing(2, 0, 2, 3),
+		overflow: 'unset'
 	},
 	postDivider: {
 		margin: theme.spacing(2, 0)
@@ -25,6 +28,25 @@ const useStyles = makeStyles(theme => createStyles({
 	},
 	postAttachment: {
 		margin: theme.spacing(2, 0)
+	},
+	postTag: {
+		padding: theme.spacing(1),
+		margin: theme.spacing(2, 0, 0, -2),
+		background: theme.palette.primary.main,
+		color: theme.palette.primary.contrastText,
+		display: 'inline-block'
+	},
+	noticeTag: {
+		background: indigo[500]
+	},
+	lectureTag: {
+		background: purple[500]
+	},
+	assignmentTag: {
+		background: teal[500]
+	},
+	referenceTag: {
+		background: deepOrange[500]
 	}
 }));
 
@@ -67,10 +89,28 @@ export const PostAttachment: React.FC<{ attachment: Attachment }> = props => {
 	);
 }
 
-export type PostComponentProps = { post: Post, action?: FetchableAction, defaultCollapsed?: boolean };
+export const PostTag: React.FC<PaperProps & { name?: string, icon: React.ReactElement }> = props => {
+	const classes = useStyles();
+	const { name, icon, children, className, ...paperProps } = props;
+
+	return (<Paper className={clsx(classes.postTag, className)} elevation={4} {...paperProps}>
+		{children ? children :
+			<Grid container direction="column" alignItems="center">
+				<Grid item>
+					{icon}
+				</Grid>
+				<Grid item>
+					<Typography variant="subtitle2" color="inherit">{name}</Typography>
+				</Grid>
+			</Grid>
+		}
+	</Paper>);
+}
+
+export type PostComponentProps = { post: Post, tag?: React.ReactElement, action?: FetchableAction, defaultCollapsed?: boolean };
 
 export const PostComponent: React.FC<PostComponentProps> = props => {
-	const { post, action, defaultCollapsed = false, children } = props;
+	const { post, tag, action, defaultCollapsed = false, children } = props;
 	const classes = useStyles();
 	const [show, setShow] = useState(!defaultCollapsed);
 
@@ -81,10 +121,15 @@ export const PostComponent: React.FC<PostComponentProps> = props => {
 
 	return (
 		<Card className={classes.post}>
-			<CardHeader
-				title={post.title}
-				subheader={<span>{subheader}{post.createdAt && <span> · <DateTime relative date={post.createdAt} /></span>}</span>}
-			/>
+			<Grid container>
+				{tag && <Grid item>{tag}</Grid>}
+				<Grid item style={{ flex: 1 }}>
+					<CardHeader
+						title={post.title}
+						subheader={<span>{subheader}{post.createdAt && <span> · <DateTime relative date={post.createdAt} /></span>}</span>}
+					/>
+				</Grid>
+			</Grid>
 
 			{show ?
 				<div>
@@ -106,14 +151,33 @@ export const PostComponent: React.FC<PostComponentProps> = props => {
 			}
 		</Card>
 	);
-};
+}
+
+export const NoticeComponent: React.FC<Omit<PostComponentProps, 'post'> & { notice: Notice }> = props => {
+	const { notice, ...postProps } = props;
+	const classes = useStyles();
+
+	return (
+		<PostComponent
+			post={notice}
+			action={actions.notice(notice.courseId, notice.id)}
+			tag={<PostTag className={classes.noticeTag} icon={<Announcement />} name="공지" />}
+			{...postProps}
+		/>
+	);
+}
 
 export const AssignmentComponent: React.FC<Omit<PostComponentProps, 'post'> & { assignment: Assignment }> = props => {
 	const { assignment, ...postProps } = props;
 	const classes = useStyles();
 
 	return (
-		<PostComponent {...postProps} post={assignment} action={actions.assignment(assignment.courseId, assignment.id)}>
+		<PostComponent
+			post={assignment}
+			action={actions.assignment(assignment.courseId, assignment.id)}
+			tag={<PostTag className={classes.assignmentTag} icon={<AssignmentLate />} name="과제" />}
+			{...postProps}
+		>
 			<CardContent>
 				{assignment.contents && <PostContent content={assignment.contents} />}
 
@@ -150,24 +214,9 @@ export const AssignmentComponent: React.FC<Omit<PostComponentProps, 'post'> & { 
 	);
 }
 
-export const NoticeComponent: React.FC<Omit<PostComponentProps, 'post'> & { notice: Notice }> = props => {
-	const { notice, ...postProps } = props;
-
-	return (
-		<PostComponent {...postProps} post={notice} action={actions.notice(notice.courseId, notice.id)} />
-	);
-}
-
-export const ReferenceComponent: React.FC<Omit<PostComponentProps, 'post'> & { reference: Reference }> = props => {
-	const { reference, ...postProps } = props;
-
-	return (
-		<PostComponent {...postProps} post={reference} action={actions.reference(reference.courseId, reference.id)} />
-	);
-}
-
 export const LectureComponent: React.FC<Omit<PostComponentProps, 'post'> & { lecture: Lecture }> = props => {
 	const { lecture, ...postProps } = props;
+	const classes = useStyles();
 	const [linkType, setLinkType] = useState('');
 	const [lazyLoad, setLazyLoad] = useState(false);
 
@@ -184,7 +233,11 @@ export const LectureComponent: React.FC<Omit<PostComponentProps, 'post'> & { lec
 	}, [lazyLoad, lecture.link]);
 
 	return (
-		<PostComponent {...postProps} post={lecture}>
+		<PostComponent
+			post={lecture}
+			tag={<PostTag className={classes.lectureTag} icon={<OndemandVideo />} name="강의" />}
+			{...postProps}
+		>
 			<VisibilitySensor onChange={isVisible => isVisible && setLazyLoad(true)}>
 				<div>
 					{lecture.link && linkType === 'html' &&
@@ -211,5 +264,19 @@ export const LectureComponent: React.FC<Omit<PostComponentProps, 'post'> & { lec
 				</div>
 			</VisibilitySensor>
 		</PostComponent>
+	);
+}
+
+export const ReferenceComponent: React.FC<Omit<PostComponentProps, 'post'> & { reference: Reference }> = props => {
+	const { reference, ...postProps } = props;
+	const classes = useStyles();
+
+	return (
+		<PostComponent
+			post={reference}
+			action={actions.reference(reference.courseId, reference.id)}
+			tag={<PostTag className={classes.referenceTag} icon={<Description />} name="자료" />}
+			{...postProps}
+		/>
 	);
 }
--- src/page/CoursePage.tsx
+++ src/page/CoursePage.tsx
@@ -94,18 +94,29 @@ export const CourseHeader: React.FC<{ course: Course }> = props => {
 export const LectureList: React.FC<{ course: Course }> = props => {
 	const { course } = props;
 	const classes = useStyles();
-	const [ activeWeek, setActiveWeek ] = useState(Object.values(course.lectures.items).length - 1);
+	const now = new Date();
+
+	const currentWeekIndex = Object.values(course.lectures.items).findIndex(lecture => {
+		return new Date(lecture.period.from) <= now && now <= new Date(lecture.period.to);
+	});
+
+	const [ activeWeek, setActiveWeek ] = useState(currentWeekIndex !== -1 ? currentWeekIndex : Object.values(course.lectures.items).length - 1);
 
 	return (
 		<FetchableList fetchableMap={course.lectures} action={actions.lectures(course.id)}>
-			<Stepper orientation="vertical" activeStep={activeWeek} nonLinear className={classes.lecturesByWeek}>
+			<Stepper
+				className={classes.lecturesByWeek}
+				orientation="vertical"
+				activeStep={activeWeek}
+				nonLinear
+			>
 				{Object.values(course.lectures.items).map((week, index) => (
 					<Step key={week.id}>
 						<StepButton
 							onClick={() => setActiveWeek(index)}
-							completed={false}
+							completed={new Date(week.period.to) < now}
 						>
-							{`${week.id}주차`}
+							{`${week.id}주차 · ${week.description}`}
 						</StepButton>
 						<StepContent>
 							{Object.values(week.items).map(lecture => (
