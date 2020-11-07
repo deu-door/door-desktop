0c131ddbb595f5286727dbd230ab6babc7172f92
solo5star <solo_5@naver.com>
2020-11-08 01:52:06 +0900

    Fix typescript type

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, Action, AnyAction, Store, Reducer } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
-import { rootReducer } from './modules';
+import { rootReducer, RootState } from './modules';
 
 // Redux Thunk 미들웨어 사용
 const middlewares = [thunk];
@@ -13,7 +13,7 @@ const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
 const enhancer = composeEnhancers(applyMiddleware(...middlewares));
 
 // Redux Store 생성
-export const store = createStore(rootReducer, enhancer);
+export const store = createStore(rootReducer as Reducer<RootState, AnyAction>, enhancer);
 
 // Redux Store 영구 저장 persistor 생성
 export const persistor = persistStore(store);
--- src/store/modules/courses.ts
+++ src/store/modules/courses.ts
@@ -164,7 +164,7 @@ export default persistReducer(
 		storage: storage,
 		transforms: [FetchableTransform]
 	},
-	handleActions<CourseState, any & never>({
+	handleActions<CourseState, any>({
 		...courseMapActions.reduxActions,
 		...courseActions.reduxActions,
 		...noticeMapActions.reduxActions,
