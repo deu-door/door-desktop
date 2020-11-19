4277d2541c191ad3cb4abb6c1f7bd9d7b9e37610
solo5star <solo_5@naver.com>
2020-11-19 22:22:20 +0900

    TeamProject: Bonus period does not exists

--- src/service/door/team-project.ts
+++ src/service/door/team-project.ts
@@ -7,8 +7,8 @@ import { doorAxios, parseInformaticTableElement, parseSubmission, parseTableElem
 export async function getTeamProject(courseId: ID, id: ID): Promise<TeamProject> {
 	const document = cheerio.load((await doorAxios.get(`/LMS/LectureRoom/CourseTeamProjectStudentDetail?CourseNo=${courseId}&ProjectNo=${id}`)).data);
 
-	const descriptionTable = document(`#sub_content2 > div:nth-child(1) > table`).toArray().pop();
-	const submissionTable = document(`#sub_content2 > div.form_table_s > table`).toArray().pop();
+	const descriptionTable = document(`#sub_content2 > div.form_table_b > table`).toArray().pop();
+	const submissionTable = document(`#CourseLeture > div.form_table_s > table`).toArray().pop();
 
 	if(!descriptionTable || !submissionTable) throw new Error(`팀 프로젝트 정보를 불러올 수 없습니다. 로그인 상태를 확인해주세요.`);
 
@@ -33,11 +33,6 @@ export async function getTeamProject(courseId: ID, id: ID): Promise<TeamProject>
 	const from = moment(description['제출기간'].text.split('~')[0].trim(), 'YY-MM-DD HH:mm').toDate();
 	const to = moment(description['제출기간'].text.split('~')[1].trim(), 'YY-MM-DD HH:mm').toDate();
 
-	const bonusPeriod = (description['추가 제출기간'].text.trim() === '없음') ? undefined : {
-		from: moment(description['추가 제출기간'].text.split('~')[0].trim(), 'YY-MM-DD HH:mm').toDate(),
-		to: moment(description['추가 제출기간'].text.split('~')[1].trim(), 'YY-MM-DD HH:mm').toDate()
-	};
-
 	return {
 		id: id,
 		courseId: courseId,
@@ -48,7 +43,6 @@ export async function getTeamProject(courseId: ID, id: ID): Promise<TeamProject>
 
 		createdAt: from,
 		period: { from, to },
-		bonusPeriod,
 
 		attachments,
 		submission,
