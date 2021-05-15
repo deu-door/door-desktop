828aa06740b64ea247daadc940e9231e7d80e07b
solo5star <solo_5@naver.com>
2021-05-15 23:52:41 +0900

    Banner.tsx: display version and notice this version is private

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -1,18 +1,23 @@
 import { Box, BoxProps, Paper, Typography } from '@material-ui/core';
+import { ErrorOutline } from '@material-ui/icons';
+import { version } from '../../../package.json';
 import React from 'react';
 
 export type BannerProps = BoxProps;
 
 export const Banner: React.FC<BannerProps> = props => {
 	const { ...otherProps } = props;
+	console.log(process.env);
 
 	return (
 		<Paper elevation={0}>
-			<Box height="72px" {...otherProps} display="flex" alignItems="center" paddingLeft="1.5rem">
-				<Typography variant="subtitle1" color="textSecondary">
-					Door Desktop 베타 테스트 버전입니다!
-				</Typography>
-			</Box>
+			<Typography variant="subtitle1" color="textSecondary">
+				<Box height="72px" {...otherProps} display="flex" alignItems="center" paddingLeft="1.5rem">
+					<ErrorOutline color="inherit" />
+					<Box width="0.5rem" />
+					Door Desktop v{version} - 비공개 테스트 버전입니다.
+				</Box>
+			</Typography>
 		</Paper>
 	);
 };
