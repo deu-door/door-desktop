3f6947727bdd7e00e9fb495b836b7980232c8f28
solo5star <solo_5@naver.com>
2021-09-04 06:10:09 +0900

    Remove door service codes (moved to deu-door/door-api)

--- src/models/door/notable.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @description 읽음 여부를 확인할 수 있는
- */
-export interface Notable {
-	noted: boolean;
-}
--- src/models/door/partible.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @description 인터페이스가 전체 정보를 포함할 수도, 아닐 수도 있음
- */
-export interface Partible {
-	partial: boolean;
-}
