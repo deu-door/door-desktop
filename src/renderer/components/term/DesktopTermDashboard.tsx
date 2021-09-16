7ca75b9b1efdd6d84a66feb01c9ac497316f3407
solo5star <solo_5@naver.com>
2021-09-16 23:37:48 +0900

    DesktopTermDashboard.tsx: separate assignments into two parts (in duration, out of duration)

--- src/renderer/components/term/DesktopTermDashboard.tsx
+++ src/renderer/components/term/DesktopTermDashboard.tsx
@@ -35,6 +35,8 @@ export const DesktopTermDashboard: React.FC<DesktopTermDashboardProps> = props =
 	const postList = _postList.filter(post => courseIds.has(post.courseId));
 	const assignmentList = _assignmentList.filter(assignment => courseIds.has(assignment.courseId));
 
+	const now = new Date().toISOString();
+
 	return courseShouldFetchSyllabus !== undefined ? (
 		<Box flex={1} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
 			<CircularProgress size={80} />
@@ -83,8 +85,8 @@ export const DesktopTermDashboard: React.FC<DesktopTermDashboardProps> = props =
 			<Grid container spacing={4}>
 				<Grid item xs={12} md={6}>
 					{/* 공지사항, 강의자료 게시물 리스트 */}
+					<Typography variant="h6">{Object.values(PostVariantNames).join(' · ')}</Typography>
 					<DesktopContentList
-						title={Object.values(PostVariantNames).join(' · ')}
 						list={postList}
 						defaultThreshold={6}
 						expandThresholdSize={8}
@@ -99,13 +101,28 @@ export const DesktopTermDashboard: React.FC<DesktopTermDashboardProps> = props =
 					/>
 				</Grid>
 				<Grid item xs={12} md={6}>
-					{/* 과제, 팀 프로젝트, 수업활동일지 게시물 리스트 */}
+					{/* 기간 내: 과제, 팀 프로젝트, 수업활동일지 게시물 리스트 */}
+					<Typography variant="h6">{Object.values(AssignmentVariantNames).join(' · ')}</Typography>
 					<DesktopContentList
-						title={Object.values(AssignmentVariantNames).join(' · ')}
-						list={assignmentList}
+						list={assignmentList.filter(assignment => now <= assignment.duration.to)}
 						VirtualListProps={{
 							defaultThreshold: 8,
 							expandThresholdSize: 8,
+							itemGroup: () => '기간 내',
+						}}
+						ListItemProps={{
+							SubtitleProps: { course: true },
+						}}
+					/>
+					<DesktopSpacer vertical={2} />
+					{/* 기간 외: 과제, 팀 프로젝트, 수업활동일지 게시물 리스트 */}
+					<DesktopContentList
+						title={Object.values(AssignmentVariantNames).join(' · ')}
+						list={assignmentList.filter(assignment => assignment.duration.to < now)}
+						VirtualListProps={{
+							defaultThreshold: 8,
+							expandThresholdSize: 8,
+							itemGroup: () => '종료됨',
 						}}
 						ListItemProps={{
 							SubtitleProps: { course: true },
